541961abed4de502ff4b01c9d277cd25e11ef86d
PC-ALEX\Utilisateur <alexandre.dutres@live.fr>
2018-11-13 02:23:25 +0100

    db test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let logger = require('morgan');
 let helmet = require('helmet');
+let connection = require(__dirname + "/lib/connect_mysql.js");
 /*Routes:*/
 let home = require('./routes/home');
 
